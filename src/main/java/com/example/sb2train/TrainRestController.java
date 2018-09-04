051f54f2dc6f9968b2b1fa4a12aa3845b7aac095
syukai <d_hirayama@rapid.ocn.ne.jp>
2018-09-04 09:13:39 +0900

    webflux

--- src/main/java/com/example/sb2train/TrainRestController.java
+++ src/main/java/com/example/sb2train/TrainRestController.java
@@ -1,15 +1,28 @@
 package com.example.sb2train;
 
+import java.util.Collections;
+import java.util.Map;
+import java.util.stream.Stream;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("train")
 public class TrainRestController {
 
-	@GetMapping
-	public String index() {
-		return "sb2!";
-	}
+    @GetMapping("/")
+    Flux<String> hello() {
+        return Flux.just("Hello", "World");
+    }
+    
+    @GetMapping("/stream")
+    Flux<Map<String, Integer>> stream() {
+        Stream<Integer> stream = Stream.iterate(0, i -> i + 1); // Java8の無限Stream
+        return Flux.fromStream(stream.limit(10))
+                .map(i -> Collections.singletonMap("value", i));
+    }
 }
