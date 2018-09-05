95e1def0eb1616a0b9f79e8722b178f3083d068d
syukai <d_hirayama@rapid.ocn.ne.jp>
2018-09-06 07:10:47 +0900

    post

--- src/main/java/com/example/sb2train/TrainRestController.java
+++ src/main/java/com/example/sb2train/TrainRestController.java
@@ -6,10 +6,13 @@ import java.util.Map;
 import java.util.stream.Stream;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("train")
@@ -26,5 +29,10 @@ public class TrainRestController {
         return Flux.fromStream(stream.limit(10))
         		.zipWith(Flux.interval(Duration.ofSeconds(1)))
                 .map(tuple -> Collections.singletonMap("value", tuple.getT1()));
+    }
+    
+    @PostMapping("/echo")
+    Mono<String> echo(@RequestBody Mono<String> body){
+    	return body.map(String::toUpperCase);
     }
 }
