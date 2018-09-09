ddaf13b27172512dcb8edb6610b4bbf8d9c0f6a0
syukai <d_hirayama@rapid.ocn.ne.jp>
2018-09-09 18:01:33 +0900

    SpringSecurity with WefFlux

--- src/main/java/com/example/sb2train/SecurityConfig.java
+++ src/main/java/com/example/sb2train/SecurityConfig.java
@@ -1,14 +1,15 @@
-//package com.example.sb2train;
+package com.example.sb2train;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+
 //
 //import org.springframework.context.annotation.Configuration;
 //import org.springframework.security.config.annotation.web.builders.WebSecurity;
 //import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-//@Configuration
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//	@Override
-//	public void configure(WebSecurity web) {
-//		web.ignoring().antMatchers("/**","/train/**");
-//	}
-//}
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+	
+}
